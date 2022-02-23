f1ab600f4639f615052fe4e8caf675c0e5b22f4f
RikkaBotWa <94896069+RikkaBotWa@users.noreply.github.com>
2022-02-23 15:52:40 +0800

    Add files via upload

--- plugins/nhdownload.js
+++ plugins/nhdownload.js
@@ -2,7 +2,7 @@ let { get } = require('axios')
 let handler = async (m, { conn, args }) => {
    if (!args[0]) throw 'Masukkan kode sebagai parameter. Berikut: #nh 304307'
   m.reply(global.wait)
-let lol = global.API('lolhum', `/api/nhentaipdf/${args[0]}`, {}, 'apikey')
+let lol = global.API('lol', `/api/nhentaipdf/${args[0]}`, {}, 'apikey')
   let { result } = (await get(lol)).data
   if (result.includes('HTML')) throw `Code ${args[0]} Not Found!`
    conn.sendMessage(m.chat, await getBuffer(result), 'documentMessage', { quoted: m, filename: `${args[0]}.pdf`, mimetype: 'application/pdf' })
@@ -12,9 +12,9 @@ handler.help = ['nhpdf'].map(v => v + ' <code>')
 
 handler.tags = ['nsfw']
 handler.command = /^(nhpdf|nhd)$/i
-handler.nsfw = true
+handler.nsfw = false
 
-handler.limit = 1
+handler.limit = 9
 
 module.exports = handler
 
