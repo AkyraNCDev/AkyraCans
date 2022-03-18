9f8bf069aa9d1834d29ed488577cb7f4b9a4b645
RikkaBotWa <94896069+RikkaBotWa@users.noreply.github.com>
2022-03-18 18:26:21 +0800

    Add files via upload

--- hentai.js
+++ hentai.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
 await m.reply(global.wait)
-  conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/nsfw/hentai?apikey=rey2k21', '', '', m)
+  conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/nsfw/hentai?apikey=AryaKey', '', '', m)
 }
 handler.help = ['hentai']
 handler.tags = ['hentai']
 handler.command = /^(hentai)$/i
-handler.limit = true
+handler.limit = 5
 handler.register = true
 module.exports = handler
